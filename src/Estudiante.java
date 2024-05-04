4046017854307f84628b832fa368758dd001529b
Anderson <anderson.blandon@hotmail.com>
2024-05-04 12:45:34 -0500

    Herencia - Sobrescritura del método toString() por Anderson

--- src/Estudiante.java
+++ src/Estudiante.java
@@ -1,5 +1,15 @@
-public class Estudiante extends Persona{
+public class Estudiante extends Persona {
 
     String GradoEstudiante;
 
+
+    @Override
+    public String toString() {
+        return "Estudiante{" +
+                "Nombre='" + getNombre() + '\'' +
+                ", FechaDeNacimiento='" + getFechaDeNacimiento() + '\'' +
+                ", Edad=" + getEdad() +
+                ", GradoEstudiante='" + GradoEstudiante + '\'' +
+                '}';
+    }
 }
