011924004630b487f1377bab7428d827962013fd
Anderson <anderson.blandon@hotmail.com>
2024-05-04 11:06:34 -0500

    Se implementaron Getters and Setters

--- src/Persona.java
+++ src/Persona.java
@@ -1,6 +1,34 @@
 public class Persona {
 
-    public String Nombre;
-    public String FechaDeNacimiento;
-    public Integer Edad;
+    private String Nombre;
+    private String FechaDeNacimiento;
+    private Integer Edad;
+
+    //Metodo constructor
+
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String nombre) {
+        Nombre = nombre;
+    }
+
+    public String getFechaDeNacimiento() {
+        return FechaDeNacimiento;
+    }
+
+    public void setFechaDeNacimiento(String fechaDeNacimiento) {
+        FechaDeNacimiento = fechaDeNacimiento;
+    }
+
+    public Integer getEdad() {
+        return Edad;
+    }
+
+    public void setEdad(Integer edad) {
+        Edad = edad;
+    }
 }
+
