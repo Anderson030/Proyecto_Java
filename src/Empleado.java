010321064e3fc70933fb4fae649f75480a2ccd09
Anderson <anderson.blandon@hotmail.com>
2024-05-04 13:42:27 -0500

    Implementación de Trabajador utilizando una clase anónima por Anderson

--- src/Empleado.java
+++ src/Empleado.java
@@ -2,5 +2,7 @@ public class Empleado implements Trabajador{
     @Override
     public void realizarTarea() {
 
+        System.out.println("Realizando tarea laboral");
+
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,14 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
-        }
+        Trabajador trabajador = new Trabajador() {
+            @Override
+            public void realizarTarea() {
+                System.out.println("Realizando tarea laboral");
+            }
+        };
+
+
+        trabajador.realizarTarea();
     }
+}
