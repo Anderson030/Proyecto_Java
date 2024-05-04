97332397826b1eb0cd6f6b4779a7e5c3893f21aa
Anderson <anderson.blandon@hotmail.com>
2024-05-04 12:26:00 -0500

    Encapsulamiento - Implementación de método adicional CalcularFechaNacimiento por Anderson

--- src/Persona.java
+++ src/Persona.java
@@ -1,11 +1,12 @@
+import java.time.Year;
+
 public class Persona {
 
     private String Nombre;
     private String FechaDeNacimiento;
     private Integer Edad;
 
-    //Metodo constructor
-
+    // Getters y setters
 
     public String getNombre() {
         return Nombre;
@@ -21,22 +22,55 @@ public class Persona {
 
     public void setFechaDeNacimiento(String fechaDeNacimiento) {
 
-        
-        FechaDeNacimiento = fechaDeNacimiento;
+        if (Edad != null && Edad >= 0) {
+
+            Year year = Year.now();
+            int añoActual = year.getValue();
+            int añoDeNacimiento = añoActual - Edad;
+
+
+
+
+            FechaDeNacimiento = fechaDeNacimiento;
+        } else {
+            System.out.println("NO VALIDO");
+        }
+
+
     }
+
 
     public Integer getEdad() {
         return Edad;
     }
 
+
+
     public void setEdad(int edad) {
         if (edad >= 0) {
             this.Edad = edad;
-
         } else {
             System.out.println("No es válida la edad");
 
+
+
+        }
+    }
+
+
+
+
+    public String calcularFechaNacimiento() {
+        if (Edad != null && Edad >= 0) {
+
+            Year year = Year.now();
+            int añoActual = year.getValue();
+            int añoDeNacimiento = añoActual - Edad;
+
+
+            return añoDeNacimiento + "-01-01";
+        } else {
+            return "No se puede calcular la fecha de nacimiento sin una edad válida.";
         }
     }
 }
-
