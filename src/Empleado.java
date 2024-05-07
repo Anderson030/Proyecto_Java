ff244ea7d6f7bcf557dc10a1fc9465f8fba84de8
Anderson <anderson.blandon@hotmail.com>
2024-05-07 10:42:37 -0500

    Herencia y Polimorfismo - Implementación de Subclases por Anderson

--- src/Empleado.java
+++ src/Empleado.java
@@ -1,9 +1,13 @@
 public class Empleado implements Trabajador{
 
+
     public String nombre;
     public int salario;
 
-
+    public Empleado(String nombre, int salario) {
+        this.nombre = nombre;
+        this.salario = salario;
+    }
 
     @Override
     public void realizarTarea() {
---
+++ src/Gerente.java
@@ -0,0 +1,5 @@
+public class Gerente  extends Empleado{
+    public Gerente(String nombre, int salario) {
+        super(nombre, salario);
+    }
+}
