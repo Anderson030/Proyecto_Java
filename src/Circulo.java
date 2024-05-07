3c67eb13f17c320ff07c7fc4815bc4f60d54dc94
Anderson <anderson.blandon@hotmail.com>
2024-05-07 10:35:24 -0500

    Encapsulamiento y Herencia - Implementación de Métodos para Cálculo de Área y Perímetro por Anderson

--- src/Circulo.java
+++ src/Circulo.java
@@ -3,8 +3,21 @@ public class Circulo extends Figura{
     private double radio;
 
 
-
-    public Circulo(String nombre, String color) {
+    public Circulo(String nombre, String color, double radio) {
         super(nombre, color);
+        this.radio = radio;
     }
-}
+
+        public double calcularArea() {
+            return Math.PI * radio * radio;
+        }
+
+
+        public double calcularPerimetro() {
+            return 2 * Math.PI * radio;
+        }
+    }
+
+
+
+
--- src/Rectangulo.java
+++ src/Rectangulo.java
@@ -7,4 +7,13 @@ public class Rectangulo extends Figura{
     public Rectangulo(String nombre, String color) {
         super(nombre, color);
     }
+
+    public double calcularArea() {
+        return longitud * ancho;
+    }
+
+    // Método para calcular el perímetro del rectángulo
+    public double calcularPerimetro() {
+        return 2 * (longitud + ancho);
+    }
 }
