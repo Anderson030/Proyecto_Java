a223082907fc2599980601f9b1febf6ccc6ae3c2
Anderson <anderson.blandon@hotmail.com>
2024-05-07 10:16:33 -0500

    Encapsulamiento y Herencia - Implementación de Métodos para Manipulación de Propiedades por Anderson

--- src/Figura.java
+++ src/Figura.java
@@ -1,2 +1,21 @@
 public class Figura {
+
+    private String nombre;
+    private String color;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
 }
